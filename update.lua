ca167d160d5bd4980c3ec7382537b7cc6ac0958c
MrWye <geo.forum@gmail.com>
2024-06-29 02:22:48 +0300

    Create update.lua

---
+++ update.lua
@@ -0,0 +1,20 @@
+local dir = "dialer"
+local root = "https://bigbass1997.com/ocprograms/"..dir.."/"
+
+os.execute("clear")
+os.execute("mkdir /"..dir)
+
+function download(filename)
+    os.execute("wget -f '"..root..filename.."' /"..dir.."/"..filename)
+end
+
+if string.find(package.path, "dialer") == nil then
+    package.path = package.path..";/"..dir.."/?.lua"
+end
+
+download("main.lua")
+download("draw.lua")
+download("icons.lua")
+download("perms.lua")
+download("update.lua")
+os.execute("/"..dir.."/main.lua")
